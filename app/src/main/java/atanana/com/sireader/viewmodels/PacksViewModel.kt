8494bb31b6e76cff84f46fe2eaeacfa17ec5c3b7
atanana <tanana@email.ru>
2018-03-18 13:30:15 +0300

    add single live event

--- app/src/main/java/atanana/com/sireader/viewmodels/PacksViewModel.kt
+++ app/src/main/java/atanana/com/sireader/viewmodels/PacksViewModel.kt
@@ -1,14 +1,13 @@
 package atanana.com.sireader.viewmodels
 
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import atanana.com.sireader.R
 import atanana.com.sireader.files.OPEN_FILE_REQUEST_CODE
 import atanana.com.sireader.files.OpenFileHandler
 
 class PacksViewModel constructor(private val openFileHandler: OpenFileHandler) : ViewModel() {
-    private val bus = MutableLiveData<Action>()
+    private val bus = SingleLiveEvent<Action>()
 
     val liveBus: LiveData<Action>
         get() = bus
