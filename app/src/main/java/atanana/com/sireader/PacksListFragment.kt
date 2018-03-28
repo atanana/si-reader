c0e0f32f756caa8b1d5a583d59b73d835440c7ff
atanana <tanana@email.ru>
2018-03-28 21:49:41 +0300

    packs list view model init

--- app/src/main/java/atanana/com/sireader/PacksListFragment.kt
+++ app/src/main/java/atanana/com/sireader/PacksListFragment.kt
@@ -4,12 +4,14 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import atanana.com.sireader.viewmodels.PacksListViewModel
 import atanana.com.sireader.viewmodels.PacksListViewModelFactory
 import atanana.com.sireader.views.gone
+import atanana.com.sireader.views.packs.PacksListAdapter
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_packs.*
 import javax.inject.Inject
@@ -22,7 +24,9 @@ class PacksListFragment : Fragment() {
     @Inject
     lateinit var viewModelFactory: PacksListViewModelFactory
 
-    lateinit var viewModel: PacksListViewModel
+    private lateinit var viewModel: PacksListViewModel
+
+    private val packsAdapter = PacksListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidSupportInjection.inject(this)
@@ -31,16 +35,22 @@ class PacksListFragment : Fragment() {
         viewModel = ViewModelProviders
                 .of(this, viewModelFactory)
                 .get(PacksListViewModel::class.java)
-
-        viewModel.files.observe(this, Observer { files ->
-            files!!
-            no_packs_label.gone(files.isNotEmpty())
-            packs_list.gone(files.isEmpty())
-        })
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_packs, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.files.observe(this, Observer { files ->
+            files!!
+            no_packs_label.gone(files.isNotEmpty())
+            packs_list.gone(files.isEmpty())
+            packsAdapter.packs = files
+        })
+
+        packs_list.layoutManager = LinearLayoutManager(activity)
+        packs_list.adapter = packsAdapter
     }
 }
---
+++ app/src/main/java/atanana/com/sireader/views/packs/PacksListAdapter.kt
@@ -0,0 +1,32 @@
+package atanana.com.sireader.views.packs
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import atanana.com.sireader.R
+import atanana.com.sireader.database.QuestionFileEntity
+
+class PacksListAdapter : RecyclerView.Adapter<PacksListAdapter.ViewHolder>() {
+    var packs = emptyList<QuestionFileEntity>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_pack, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = packs.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val pack = packs[position]
+        holder.packName.text = pack.title
+        holder.packFile.text = pack.filename
+    }
+
+    class ViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val packName: TextView = item.findViewById(R.id.pack_name)
+        val packFile: TextView = item.findViewById(R.id.pack_name)
+    }
+}
