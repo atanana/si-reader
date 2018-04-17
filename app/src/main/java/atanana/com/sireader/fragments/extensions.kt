a427d4147ae94e8735819c412a8fc815c1f38156
atanana <tanana@email.ru>
2018-04-17 13:31:52 +0300

    fragment animations init

--- app/src/main/java/atanana/com/sireader/fragments/extensions.kt
+++ app/src/main/java/atanana/com/sireader/fragments/extensions.kt
@@ -5,6 +5,7 @@ import atanana.com.sireader.R
 
 fun FragmentManager.openFragment(fragment: BaseFragment) {
     beginTransaction()
+            .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right)
             .replace(R.id.fragment, fragment, fragment.transactionTag)
             .addToBackStack(fragment.transactionTag)
             .commit()
