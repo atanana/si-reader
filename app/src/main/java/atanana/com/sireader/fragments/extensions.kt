02b4a14f0ccdc9161ba819abeb52d1eb39e97c5d
Andrei Tanana <tanana@email.ru>
2021-02-24 22:33:36 +0300

    remove fragment animations

--- app/src/main/java/atanana/com/sireader/fragments/extensions.kt
+++ app/src/main/java/atanana/com/sireader/fragments/extensions.kt
@@ -8,12 +8,6 @@ import atanana.com.sireader.R
 
 fun FragmentManager.openFragment(fragment: BaseFragment<*>, addToBackStack: Boolean = true) {
     val transaction = beginTransaction()
-        .setCustomAnimations(
-            if (addToBackStack) R.anim.enter_from_right else 0,
-            R.anim.exit_to_left,
-            R.anim.enter_from_left,
-            R.anim.exit_to_right
-        )
         .replace(R.id.fragment, fragment, fragment.transactionTag)
     if (addToBackStack) {
         transaction.addToBackStack(fragment.transactionTag)
