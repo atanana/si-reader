eff0f3faec7219016a1d82a595408d28e0b3834c
Andrei Tanana <tanana@email.ru>
2021-03-05 15:29:40 +0300

    migrate to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,110 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+    id("com.getkeepsafe.dexcount")
+    id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
+    id("io.gitlab.arturbosch.detekt").version("1.16.0-RC2")
+}
+
+android {
+    compileSdkVersion(30)
+    defaultConfig {
+        applicationId("atanana.com.sireader")
+        minSdkVersion(21)
+        targetSdkVersion(30)
+        versionCode(1)
+        versionName("1.0")
+        testInstrumentationRunner("android.support.test.runner.AndroidJUnitRunner")
+        vectorDrawables.useSupportLibrary = true
+
+        kapt {
+            arguments {
+                arg("room.schemaLocation", "$projectDir/schemas")
+            }
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        getByName("debug") {
+            multiDexEnabled = true
+        }
+
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles("proguard-rules.pro", getDefaultProguardFile("proguard-android.txt"))
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dexcount {
+    format = "list"
+    includeClasses = false
+    includeClassCount = false
+    includeFieldCount = true
+    includeTotalMethodCount = false
+    orderByMethodCount = false
+    verbose = false
+    maxTreeDepth = Integer.MAX_VALUE
+    teamCityIntegration = false
+    teamCitySlug = null
+    runOnEachPackage = true
+    maxMethodCount = 640000
+}
+
+detekt {
+    toolVersion = "1.16.0-RC2"
+    config = files("config/detekt/detekt.yml")
+    buildUponDefaultConfig = true
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${rootProject.extra.get("kotlinVersion")}")
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.1.0")
+    implementation("androidx.activity:activity-ktx:1.2.0")
+    implementation("androidx.fragment:fragment-ktx:1.3.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0")
+
+    implementation("com.github.Zhuinden:fragmentviewbindingdelegate-kt:1.0.0")
+
+    val hiltVersion = rootProject.extra.get("hiltVersion")
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
+
+    val roomVersion = "2.2.6"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
+    val navVersion = rootProject.extra.get("navVersion")
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    implementation("org.apache.poi:poi-scratchpad:3.17")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+buildscript {
+    val kotlinVersion by rootProject.extra("1.4.31")
+    val hiltVersion by rootProject.extra("2.32-alpha")
+    val navVersion by rootProject.extra("2.3.3")
+
+    repositories {
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.getkeepsafe.dexcount:dexcount-gradle-plugin:2.0.0")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven("https://jitpack.io")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
