e33da964bacc4eeb0b9bd8367d8ed66cba2882c1
Andrei Tanana <tanana@email.ru>
2021-05-13 21:17:09 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,10 +82,10 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.1.0")
-    implementation("androidx.activity:activity-ktx:1.2.1")
-    implementation("androidx.fragment:fragment-ktx:1.3.1")
+    implementation("androidx.activity:activity-ktx:1.2.3")
+    implementation("androidx.fragment:fragment-ktx:1.3.3")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
 
     implementation("com.github.Zhuinden:fragmentviewbindingdelegate-kt:1.0.0")
 
